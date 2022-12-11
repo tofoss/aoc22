629df3ee6cafea3d68e8c6e013055fc2b92dcbb2
Tormod Fossum <tormod.fossum@nav.no>
2022-12-11 16:30:44 +0100

    Day 11 part 2

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -4,18 +4,16 @@ const DAY: &str = "11";
 
 #[derive(Debug, Clone)]
 struct Expr {
-    left: Option<i32>,
-    right: Option<i32>,
+    left: Option<u64>,
+    right: Option<u64>,
     operator: char,
 }
 
 impl Expr {
-    fn eval(&self) -> i32 {
+    fn eval(&self) -> u64 {
         return match self.operator {
             '+' => self.left.unwrap() + self.right.unwrap(),
-            '-' => self.left.unwrap() - self.right.unwrap(),
             '*' => self.left.unwrap() * self.right.unwrap(),
-            '/' => self.left.unwrap() / self.right.unwrap(),
             _ => panic!("unsupported operator")
         }
     }
@@ -25,27 +23,27 @@ impl FromStr for Expr {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        fn const_or_old(s: &str) -> Option<i32> {
+        fn const_or_old(s: &str) -> Option<u64> {
             if s == "old" { return None; } 
-            return Some(s.parse::<i32>().unwrap());
+            return Some(s.parse::<u64>().unwrap());
         }
         let mut values = s.split(" ").into_iter();
-        let left: Option<i32> = const_or_old(values.next().unwrap().trim());
+        let left: Option<u64> = const_or_old(values.next().unwrap().trim());
         let operator: char = values.next().unwrap().trim().chars().next().unwrap();
-        let right: Option<i32> = const_or_old(values.next().unwrap().trim());
+        let right: Option<u64> = const_or_old(values.next().unwrap().trim());
         Ok(Expr { left, right, operator })
     }
 }
 
 #[derive(Debug)]
 struct Test {
-    divisible: i32,
+    divisible: u64,
     if_true: usize,
     if_false: usize,
 }
 
 impl Test {
-    fn eval(&self, v: i32) -> usize {
+    fn eval(&self, v: u64) -> usize {
         if v % self.divisible == 0 {
             return self.if_true;
         }
@@ -73,14 +71,14 @@ fn to_test(lines: Vec<&str>) -> Test {
 
 #[derive(Debug)]
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<u64>,
     operation: Expr,
     test: Test,
-    held_item: Option<i32>,
+    held_item: Option<u64>,
 }
 
 impl Monkey {
-    fn inspect(&mut self) {
+    fn inspect(&mut self, prod: u64) {
         let item = self.items.pop().unwrap();
         let mut expr = self.operation.clone();
 
@@ -90,14 +88,14 @@ impl Monkey {
         if expr.right == None {
             expr.right = Some(item)
         }
-        self.held_item = Some(expr.eval() / 3);
+        self.held_item = Some(expr.eval() % prod);
     }
 
     fn target_monkey(&self) -> usize {
         return self.test.eval(self.held_item.unwrap())
     }
 
-    fn throw(&mut self) -> i32 {
+    fn throw(&mut self) -> u64 {
         let i = self.held_item.unwrap();
         self.held_item = None;
         return i;
@@ -111,19 +109,15 @@ impl FromStr for Monkey {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut lines = s.lines().skip(1).into_iter();
 
-        let mut items: Vec<i32> = lines
+        let mut items: Vec<u64> = lines
             .next()
             .unwrap()
             .split_once(":")
-            .unwrap().1.split(",").flat_map(|n| n.trim().parse::<i32>()).collect();
+            .unwrap().1.split(",").flat_map(|n| n.trim().parse::<u64>()).collect();
 
         let expr: Expr = lines.next().unwrap().split_once("=").unwrap().1.trim().parse().unwrap();
 
         let test: Test = to_test(lines.collect());
-
-        println!("{:?}", items);
-        println!("{:?}", expr);
-        println!("{:?}", test);
 
         items.reverse();
 
@@ -131,14 +125,15 @@ impl FromStr for Monkey {
     }
 }
 
-fn monkey_business(monkies: &mut Vec<Rc<RefCell<Monkey>>>, rounds: usize) -> i32 {
+fn monkey_business(monkies: &mut Vec<Rc<RefCell<Monkey>>>, rounds: usize) -> usize {
     let mut inspections = vec![0; monkies.len()];
+    let prod: u64 = monkies.iter().map(|m| m.borrow().test.divisible).product();
 
     for _ in 0..rounds {
         for (i, m) in monkies.iter().enumerate() {
             let mut cur = m.borrow_mut();
             for _ in 0..cur.items.len(){
-                cur.inspect();
+                cur.inspect(prod);
                 let mut tm = monkies[cur.target_monkey()].borrow_mut();
                 tm.items.push(cur.throw());
                 inspections[i] += 1;
@@ -160,16 +155,20 @@ pub fn solve() {
     #[allow(unused_variables)]
     let prod = format!("input/day{DAY}.txt");
 
-    let mut input: Vec<Rc<RefCell<Monkey>>> = fs::read_to_string(test).unwrap().split("\n\n").map(|s| Rc::new(RefCell::new(s.parse::<Monkey>().unwrap()))).collect();
+    let mut input: Vec<Rc<RefCell<Monkey>>> = fs::read_to_string(prod)
+        .unwrap()
+        .split("\n\n")
+        .map(|s| Rc::new(RefCell::new(s.parse::<Monkey>().unwrap())))
+        .collect();
 
         
     println!("Dec {DAY}:");
-    part_one(&mut input);
+    part_two(&mut input);
     println!();
 }
 
-fn part_one(monkies: &mut Vec<Rc<RefCell<Monkey>>>) {
-    let result = monkey_business(monkies, 20);
+fn part_two(monkies: &mut Vec<Rc<RefCell<Monkey>>>) {
+    let result = monkey_business(monkies, 10_000);
 
-    println!("Part one answer: {result}")
+    println!("Part two answer: {result}")
 }
